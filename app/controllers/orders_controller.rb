7e86707b3a8f5a3fe40d5846416c8019ff9b8a24
Gerson <gersonwacherski@gmail.com>
2024-01-29 20:04:41 -0300

    orders controller and views

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,48 @@
 class OrdersController < ApplicationController
+  before_action :set_user, :set_book, only: [:new, :create]
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def new
+    @order = @book.orders.build
+  end
+
+  def create
+    @order = @book.orders.build(order_params)
+    if @order.save
+      redirect_to @order, notice: "Ordem criada com sucesso."
+    else
+     render :new, notice: "Falha ao criar pedido, tente novamente."
+    end
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to @order, notice: "Ordem atualizada com sucesso."
+    else
+      render :edit, notice: "Falha ao atualizar pedido, tente novamente."
+    end
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+
+  def order_params
+    params.require(:order).permit(:quantity, :total_price)
+  end
+
+  def set_user
+    @user = current_user
+  end
+
 end
