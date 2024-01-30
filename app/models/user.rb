cc1371a2181b71314de3213bb44b7d6c5b420b1b
Luiz Kaiser <lfpkaiser@gmail.com>
2024-01-30 19:07:55 -0300

    added cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :orders
   has_many :books
 
+  has_one_attached :photo
+
   validates :username, presence: true, uniqueness: true
   validates :first_name, :last_name, presence: true
 
