927395f4a9b84ecb9d27edfdc1e07d4345d05f99
Luiz Kaiser <lfpkaiser@gmail.com>
2024-01-31 20:39:19 -0300

    added seeds for 3 users and 5 books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
+  GENRES = ['Fantasy', 'Sci-Fi', 'Mystery', 'Biography', 'History', 'Romance',
+            'Horror', 'Thriller', 'Non-fiction', 'Fiction', 'Children',
+            'Self-help', 'Cooking']
+
   belongs_to :user
   has_many :orders
 
@@ -6,7 +10,7 @@ class Book < ApplicationRecord
 
   validates :name, presence: true
   validates :author, presence: true
-  validates :genre, presence: true
+  validates :genre, presence: true, inclusion: { in: GENRES }
   validates :year, presence: true, numericality: { only_integer: true }
   validates :price, presence: true, numericality: { only_integer: true }
   validates :quantity, presence: true, numericality: { only_integer: true }
