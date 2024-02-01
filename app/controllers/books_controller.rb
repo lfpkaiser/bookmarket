09eccb6e61e5842a4d10646981c88982e5fe3a4e
kelle oliveira <kellemonik@gmail.com>
2024-01-31 21:45:39 -0300

    adding image upload for books index and show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,6 +32,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:name, :author, :genre, :year, :price, :quantity)
+    params.require(:book).permit(:name, :author, :genre, :year, :price, :quantity, :photo)
   end
 end
