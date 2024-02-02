331fd3d9f055faff76fb0afbb77662a023e9d50e
kelle oliveira <kellemonik@gmail.com>
2024-02-02 20:53:19 -0300

    new photo size

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,9 +29,29 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
 
+  def buy
+    @book = Book.find(params[:id])
+    @order = Order.new(order_params)
+    @order.book = @book
+    @order.user = current_user
+
+    if @order.quantity <= @book.quantity
+      @order.save
+      @book.quantity -= @order.quantity
+      @book.save
+      redirect_to books_path, notice: 'Order placed successfully.'
+    else
+      redirect_to books_path, alert: 'Insufficient quantity.'
+    end
+  end 
+
   private
 
   def book_params
     params.require(:book).permit(:name, :author, :genre, :year, :price, :quantity, :photo)
+  end
+
+  def order_params
+    params.require(:order).permit(:quantity)
   end
 end
